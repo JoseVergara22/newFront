33a7f91770e8675be222cdd3a7132cfc3e4cba03
witceo <ceo@witup.co>
2020-05-23 23:24:58 -0500

    compilado para dev

--- src/app/master/master-settlement-all/master-settlement-all.component.ts
+++ src/app/master/master-settlement-all/master-settlement-all.component.ts
@@ -3899,7 +3899,7 @@ this.getImgFromUrl(logo_url, function (img) {
 
  updateEstimate(row:any){
    console.log(row);
-   this.router.navigateByUrl('maintenance/estimateCustomerUpdate/'+row.id);
+   this.router.navigateByUrl('maintenance/settlementCustomerUpdate/'+row.id);
  }
 
  copyEstimateProcess(row:any){
