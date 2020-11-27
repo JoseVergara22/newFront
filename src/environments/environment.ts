5607f65fc6a54534feaaccef047c93b467ff6197
witceo <ceo@witup.co>
2020-11-27 13:25:22 -0500

    comp test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,11 +2,13 @@
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
+// Dominio https://montacargasmasterbackdev.co
+// Ip http://34.207.70.171/
 
 export const environment = {
   production: false,
   name:"(DEV)",
-  apiBaseUrl:"http://34.207.70.171/",
+  apiBaseUrl:"https://montacargasmasterbackdev.co/",
   clientId: '1',
   clientSecret: 'TyG8pFzcGI7nTIt31FzFCwnvF0E8t70OSAIyrwWT',
 };
